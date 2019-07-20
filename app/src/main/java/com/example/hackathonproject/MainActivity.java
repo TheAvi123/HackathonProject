181bb0a01bae3952b2daa828f4219583fcdc4279
siddharthkunche <siddharth.kuncheua@gmail.com>
2019-07-20 12:18:16 -0700

    Added buttons on main menu, both buttons cause the game to exit; plan to update functionality on start the game button to open up a menu

--- app/src/main/java/com/example/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/hackathonproject/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.hackathonproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button startGame,quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startGame = (Button) findViewById(R.id.GameStart);
+        quit = (Button) findViewById(R.id.ExitGame);
+        createOnClickListeners();
+    }
+
+    private void createOnClickListeners() {
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hackathonproject/Sprite.java
+++ app/src/main/java/com/example/hackathonproject/Sprite.java
@@ -1,4 +1,5 @@
 package com.example.hackathonproject;
 
 public abstract class Sprite {
+
 }
